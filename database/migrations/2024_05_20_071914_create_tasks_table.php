716fe772d8ce075af8edccebc1125e1f111c638f
msandypr <msandypr@protonmail.com>
2024-05-20 14:38:17 +0700

    feat: user controller

--- database/migrations/2024_05_20_071914_create_tasks_table.php
+++ database/migrations/2024_05_20_071914_create_tasks_table.php
@@ -11,14 +11,15 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('tasks', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->boolean('admin')->default(0);
-            $table->rememberToken();
+            $table->foreignId('taskcreator_id');
+            $table->foreignId('assigneduser_id');
+            $table->string('title');
+            $table->string('slug');
+            $table->text('description');
+            $table->dateTime('due');
+            $table->boolean('completed');
             $table->timestamps();
         });
     }
@@ -28,6 +29,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('tasks');
     }
 };
