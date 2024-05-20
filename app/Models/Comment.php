ca9953d7506c770b60bf3fcf7f3115839ac1f2b3
msandypr <msandypr@protonmail.com>
2024-05-20 18:26:26 +0700

    fix: comment doesnt work because the body not allowing mass assignment in the comment model

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -9,6 +9,8 @@ class Comment extends Model
 {
     use HasFactory;
 
+    protected $fillable = ['body', 'task_id', 'user_id'];
+
     public function task()
     {
         return $this->belongsTo(Task::class);
