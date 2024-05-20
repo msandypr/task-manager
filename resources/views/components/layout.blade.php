710d112d625f2d063918d56f4429d3ca998ebde4
msandypr <msandypr@protonmail.com>
2024-05-20 19:52:24 +0700

    refactor: dashboard layout

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,184 +1,116 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tailwind Admin Template</title>
-    <meta name="author" content="David Grzyb">
-    <meta name="description" content="">
+    <title>Task Manager</title>
+    <meta name="author" content="msandypr">
+    <meta name="description" content="task-manager">
 
     <!-- Tailwind -->
-    <link href="https://unpkg.com/tailwindcss/dist/tailwind.min.css" rel="stylesheet">
+    <script src="https://cdn.tailwindcss.com"></script>
+
     <style>
         @import url('https://fonts.googleapis.com/css?family=Karla:400,700&display=swap');
-        .font-family-karla { font-family: karla; }
-        .bg-sidebar { background: #3d68ff; }
-        .cta-btn { color: #3d68ff; }
-        .upgrade-btn { background: #1947ee; }
-        .upgrade-btn:hover { background: #0038fd; }
-        .active-nav-link { background: #1947ee; }
-        .nav-item:hover { background: #1947ee; }
-        .account-link:hover { background: #3d68ff; }
+
+        .font-family-karla {
+            font-family: karla;
+        }
+
+        .cta-btn {
+            color: #3d68ff;
+        }
+
+        .upgrade-btn {
+            background: #1947ee;
+        }
+
+        .upgrade-btn:hover {
+            background: #0038fd;
+        }
+
+        .nav-item:hover {
+            background: #1947ee;
+        }
+
+        .account-link:hover {
+            background: #3d68ff;
+        }
     </style>
 </head>
-<body class="bg-gray-100 font-family-karla w-auto flex">
 
-    <aside class="relative bg-blue-600 w-54 shadow-xl">
-        <div class="p-6">
-            <a href="{{ route('task.index')}}" class="text-white text-3xl font-semibold uppercase hover:text-gray-300">Task Manager</a>
-            <button class="w-full bg-white cta-btn font-semibold py-2 mt-5 rounded-br-lg rounded-bl-lg rounded-tr-lg shadow-lg hover:shadow-xl hover:bg-gray-300 flex items-center justify-center">
-                <i class="fas fa-plus mr-3"></i><a href="{{ route('task.create') }}">New Task</a>
-            </button>
+<body class="bg-gray-100 font-family-karla w-auto flex flex-col">
+
+    <!-- Navbar -->
+    <header class="w-full bg-white py-4 px-6 flex items-center justify-between">
+        <div>
+            <a href="{{ route('task.index') }}" class="text-blue-600 text-3xl font-semibold uppercase hover:text-gray-300">Task Manager</a>
         </div>
-        <nav class="text-white text-base font-semibold pt-3">
-            <a href="{{ route('admin.dashboard') }}" class="flex items-center  text-white py-4 pl-6 nav-item">
-                <i class="fas fa-tachometer-alt mr-3"></i>
-                Admin Dashboard
-            </a>
+        <nav class="flex items-center space-x-4">
             @auth
-            <a href="/user/{{auth()->id()}}/dashboard" class="flex items-center text-white py-4 pl-6 nav-item">
-                <i class="fas fa-user mr-3"></i>
-                 {{ ucwords(auth()->user()->name) }}'s Dashboard
-            </a>
+                @if(Auth::user()->admin)
+                   <a href="{{ route('admin.dashboard') }}" class="flex items-center text-blue-600 hover:text-gray-700">
+                    <i class="fas fa-tachometer-alt mr-2"></i>
+                    Admin Dashboard
+                    </a>
+                @endif
+                <a href="/user/{{ auth()->id() }}/dashboard" class="flex items-center text-blue-600 hover:text-gray-700">
+                    <i class="fas fa-user mr-2"></i>
+                    {{ ucwords(auth()->user()->name) }}'s Dashboard
+                </a>
             @endauth
-
-            <a href="{{ route('task.index')}}" class="flex items-center text-white hover:opacity-100 py-4 pl-6 nav-item">
-                <i class="fas fa-tasks mr-3"></i>
+            <a href="{{ route('task.index') }}" class="flex items-center text-blue-600 hover:text-gray-700">
+                <i class="fas fa-tasks mr-2"></i>
                 Tasks
             </a>
-        </nav>
-    </aside>
-
-    <div class="w-full flex flex-col h-screen overflow-y-hidden">
-        <!-- Desktop Header -->
-        <header class="w-full items-center bg-white py-2 px-6 sm:flex">
-            <div class=" row w-1/2"> </div>
             @auth
-            <div x-data="{ isOpen: false }" class="relative w-1/2 flex justify-end">
-                <button @click="isOpen = !isOpen" class="align-content relative z-10 w-auto h-10 mr-4 font-bold">
-                     {{ ucwords(auth()->user()->name) }}
-                </button>
-                <button x-show="isOpen" @click="isOpen = false" class="h-full w-full fixed inset-0 cursor-default"></button>
-                <div x-show="isOpen" class="absolute w-25 bg-white rounded-lg shadow-lg py-2 mt-6">
-                    <a href="#" class="block px-4 py-2 account-link hover:text-white">Account</a>
-                    <form action="/logout" method="post" class="block px-4 py-2 account-link hover:text-white">
-                        @csrf
-                          <button>Logout</button>
-                    </form>
+                <div x-data="{ isOpen: false }" class="relative">
+                    <button @click="isOpen = !isOpen" class="relative z-10 w-auto h-10 font-bold">
+                        {{ ucwords(auth()->user()->name) }}
+                    </button>
+                    <button x-show="isOpen" @click="isOpen = false" class="h-full w-full fixed inset-0 cursor-default"></button>
+                    <div x-show="isOpen" class="absolute right-0 w-48 bg-white rounded-lg shadow-lg py-2 mt-2">
+                        <a href="/user/{{ auth()->id() }}/dashboard" class="block px-4 py-2 account-link hover:text-white">Account</a>
+                        <form action="/logout" method="post" class="block px-4 py-2 account-link hover:text-white">
+                            @csrf
+                            <button>Logout</button>
+                        </form>
+                    </div>
                 </div>
-            </div>
             @endauth
             @guest
-            <div class="relative w-1/2 flex justify-end">
-                <a href="/register" class="block px-4 py-2 account-link hover:text-white">Register</a>
-                <a href="/login" class="block px-4 py-2 account-link hover:text-white">Login</a>
-            </div>
-            @endguest
-        </header>
-
-        <div class="w-full overflow-x-hidden border-t flex flex-col">
-            <main class="w-auto flex-grow p-6">
-                 {{-- Flash Sesion --}}
-                @if (session('success'))
-                <div class="text-blue font-bold">
-                    {{ session('success') }}
+                <div>
+                    <a href="/register" class="px-4 py-2 account-link hover:text-white">Register</a>
+                    <a href="/login" class="px-4 py-2 account-link hover:text-white">Login</a>
                 </div>
-                @endif
+            @endguest
+        </nav>
+    </header>
 
-                    {{$slot}}
-            </main>
-
-            <footer class="w-full bg-white text-right p-4">
-                Built by <a target="_blank" href="#" class="underline">Wassilolly</a>.
-            </footer>
+    @if (session()->has('success'))
+        <div x-data="{ show:true}" x-show="show" x-init="setTimeout(() => show = false, 4000)"
+            class="fixed top-0 left-1/2 transform -translate-x-1/2 bg-blue-500 text-white py-2 px-4 w-1/4 rounded-xl text-sm">
+            <p>{{ session('success') }}</p>
         </div>
+    @endif
 
+    <!-- Main Content -->
+    <div class="w-full overflow-x-hidden border-t flex flex-col">
+        <main class="w-auto flex-grow p-6">
+            {{ $slot }}
+        </main>
+
+        <footer class="w-full bg-white text-right p-4">
+            Built by <a target="_blank" href="#" class="underline">MSANDYPR</a>.
+        </footer>
     </div>
-
 
     <!-- AlpineJS -->
     <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
     <!-- Font Awesome -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/js/all.min.js" integrity="sha256-KzZiKy0DWYsnwMF+X1DvQngQ2/FxF7MF3Ff72XcpuPs=" crossorigin="anonymous"></script>
-    <!-- ChartJS -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.js" integrity="sha256-R4pqcOYV8lt7snxMQO/HSbVCFRPMdrhAFMH+vr9giYI=" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/js/all.min.js"
+        integrity="sha256-KzZiKy0DWYsnwMF+X1DvQngQ2/FxF7MF3Ff72XcpuPs=" crossorigin="anonymous"></script>
 
-    <script>
-        var chartOne = document.getElementById('chartOne');
-        var myChart = new Chart(chartOne, {
-            type: 'bar',
-            data: {
-                labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-                datasets: [{
-                    label: '# of Votes',
-                    data: [12, 19, 3, 5, 2, 3],
-                    backgroundColor: [
-                        'rgba(255, 99, 132, 0.2)',
-                        'rgba(54, 162, 235, 0.2)',
-                        'rgba(255, 206, 86, 0.2)',
-                        'rgba(75, 192, 192, 0.2)',
-                        'rgba(153, 102, 255, 0.2)',
-                        'rgba(255, 159, 64, 0.2)'
-                    ],
-                    borderColor: [
-                        'rgba(255, 99, 132, 1)',
-                        'rgba(54, 162, 235, 1)',
-                        'rgba(255, 206, 86, 1)',
-                        'rgba(75, 192, 192, 1)',
-                        'rgba(153, 102, 255, 1)',
-                        'rgba(255, 159, 64, 1)'
-                    ],
-                    borderWidth: 1
-                }]
-            },
-            options: {
-                scales: {
-                    yAxes: [{
-                        ticks: {
-                            beginAtZero: true
-                        }
-                    }]
-                }
-            }
-        });
-
-        var chartTwo = document.getElementById('chartTwo');
-        var myLineChart = new Chart(chartTwo, {
-            type: 'line',
-            data: {
-                labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-                datasets: [{
-                    label: '# of Votes',
-                    data: [12, 19, 3, 5, 2, 3],
-                    backgroundColor: [
-                        'rgba(255, 99, 132, 0.2)',
-                        'rgba(54, 162, 235, 0.2)',
-                        'rgba(255, 206, 86, 0.2)',
-                        'rgba(75, 192, 192, 0.2)',
-                        'rgba(153, 102, 255, 0.2)',
-                        'rgba(255, 159, 64, 0.2)'
-                    ],
-                    borderColor: [
-                        'rgba(255, 99, 132, 1)',
-                        'rgba(54, 162, 235, 1)',
-                        'rgba(255, 206, 86, 1)',
-                        'rgba(75, 192, 192, 1)',
-                        'rgba(153, 102, 255, 1)',
-                        'rgba(255, 159, 64, 1)'
-                    ],
-                    borderWidth: 1
-                }]
-            },
-            options: {
-                scales: {
-                    yAxes: [{
-                        ticks: {
-                            beginAtZero: true
-                        }
-                    }]
-                }
-            }
-        });
-    </script>
 </body>
 </html>
